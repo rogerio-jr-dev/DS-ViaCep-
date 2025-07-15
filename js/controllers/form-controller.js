1d15d9737fe882947c6da2b72d01c18f93d4de6a
rogerio-jr-dev <ethicalhacker.dev@gmail.com>
2025-07-15 20:25:26 -0300

    Implemented JavaScript

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,66 +1,118 @@
-import Adress from '../models/adress.js';
+import Address from '../models/address.js';
+import * as addressService from '../services/address-service.js';
+import * as listController from './list-controller.js';
 
-function State(){
-    this.adress = new Adress();
+function State() {
 
-    this.btnClear = null;
+    this.address = new Address();
+
     this.btnSave = null;
+    this.btnClear = null;
 
-     this.inputCep = null;
-     this.inputStreet = null;
-     this.inputNumber = null;
-     this.inputCity = null;
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
 
-     this.errorCep = null;
-     this.errorNumber = null;
-
+    this.errorCep = null;
+    this.errorNumber = null;
 }
 
 const state = new State();
 
 export function init() {
-    state.inputCep = document.forms.newAdress.cep;
-    state.inputStreet = document.forms.newAdress.street;
-    state.inputNumber = document.forms.newAdress.number;
-    state.inputCity = document.forms.newAdress.city;
 
-    state.btnClear = document.forms.newAdress.btnClear;
-    state.btnSave = document.forms.newAdress.btnSave;
+    state.inputCep = document.forms.newAddress.cep;
+    state.inputStreet = document.forms.newAddress.street;
+    state.inputNumber = document.forms.newAddress.number;
+    state.inputCity = document.forms.newAddress.city;
+
+    state.btnSave = document.forms.newAddress.btnSave;
+    state.btnClear = document.forms.newAddress.btnClear;
 
     state.errorCep = document.querySelector('[data-error="cep"]');
     state.errorNumber = document.querySelector('[data-error="number"]');
 
-    state.inputNumber.addEventListener('change', hendleInputChanceNumber);
-    state.btnClear.addEventListener('click', hendleBtnClearClick)
+    state.inputNumber.addEventListener('change', handleInputNumberChange);
+    state.inputNumber.addEventListener('keyup', handleInputNumberKeyup);
+    state.btnClear.addEventListener('click', handleBtnClearClick);
+    state.btnSave.addEventListener('click', handleBtnSaveClick);
+    state.inputCep.addEventListener('change', handleInputCepChange);
 }
-function hendleInputChanceNumber(event){
+
+function handleInputNumberKeyup(event) {
+    state.address.number = event.target.value;
+}
+
+async function handleInputCepChange(event) {
+    const cep = event.target.value;
+
+    try {
+        const address = await addressService.findByCep(cep);
+
+        state.inputStreet.value = address.street;
+        state.inputCity.value = address.city;
+        state.address = address;
+
+        setFormError("cep", "");
+        state.inputNumber.focus();
+    }
+    catch (e) {
+        state.inputStreet.value = "";
+        state.inputCity.value = "";
+        setFormError("cep", "Informe um CEP válido");
+    }
+}
+
+function handleBtnSaveClick(event) {
+    event.preventDefault();
+
+    const errors = addressService.getErrors(state.address);
+
+    const keys = Object.keys(errors);
+
+    if (keys.length > 0) {
+        keys.forEach(key => {
+            setFormError(key, errors[key]);
+        });
+    }
+    else {
+        listController.addCard(state.address);
+        clearForm();
+    }
+}
+
+function handleInputNumberChange(event) {
     if (event.target.value == "") {
         setFormError("number", "Campo requerido");
     }
-    else{
+    else {
         setFormError("number", "");
     }
 }
 
-function hendleBtnClearClick(event){
+function handleBtnClearClick(event) {
     event.preventDefault();
     clearForm();
 }
 
-function clearForm(){
+function clearForm() {
+    console.log(state);
+
     state.inputCep.value = "";
-    state.inputStreet.value = "";
-    state.inputNumber.value = "";
     state.inputCity.value = "";
+    state.inputNumber.value = "";
+    state.inputStreet.value = "";
 
     setFormError("cep", "");
     setFormError("number", "");
 
-    state.inputCep.focus();
+    state.address = new Address();
 
+    state.inputCep.focus();
 }
 
-function setFormError(key, value){
+function setFormError(key, value) {
     const element = document.querySelector(`[data-error="${key}"]`);
     element.innerHTML = value;
 }
